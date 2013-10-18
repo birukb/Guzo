e834056ca512c7540e58b62bca3a2c6645b374b6
birukb <biruk_belachew@yahoo.com>
2013-10-18 00:40:45 -0700

    fancy refactoring on the header directive; a whole lot more to come

--- app/scripts/directives/header.js
+++ app/scripts/directives/header.js
@@ -8,8 +8,6 @@ define([], function (){
             templateUrl:'partials/header.html',
             replace: true,
             link: function(scope, element){
-                var displayed;
-
                 scope.nav = {
                     maps: HeaderMenu.anonHeaderMenu
                 };
@@ -18,49 +16,13 @@ define([], function (){
                     title: 'Guzo'
                 };
 
+                scope.isActive = function(page){
+                    return page === $location.path().substr(1);  // url has a leading '/'
+                };
+
                 $rootScope.$on('$routeChangeStart', function (){
-                    highlightCurrentLocation();
+                    element.find('.nav-collapse').collapse('hide');
                 });
-
-                function highlightCurrentLocation(){
-                    function getCurrentLocation(){
-                        var i,
-                            len = scope.nav.maps.length,
-                            url = $location.url().substr(1);  // url has a leading '/'
-
-                        for(i = 0; i< len; i++){
-                            if(scope.nav.maps[i].url === url){
-                                return scope.nav.maps[i];
-                            }
-                        }
-                    }
-
-                    function collapseNav (){
-                        // hack; bootstrap hide has bug that it shows when told to hide
-                        var $nav = element.find('.nav-collapse');
-                        if($nav.height() > 0){
-                            $nav.collapse('hide');
-                        }
-                    }
-
-                    var map = getCurrentLocation();
-                    if(displayed){
-                        displayed.class = null;
-                    }
-
-                    if(map){
-                        map.class = 'active';
-                        displayed = map;
-                    }
-                    else{
-                        displayed = null;
-                    }
-
-                    collapseNav();
-                }
-
-                // taking care of bookmarked pages
-                highlightCurrentLocation();
             }
         };
     };
